c0554f3438971099fa063bbdcb02c149f0715f58
youssef ahmed <youssefahmedabdelfattah@outlook.com>
2023-10-15 20:12:32 +0300

    Create Base Dto

--- Fakrny.Api/GloablUsings.cs
+++ Fakrny.Api/GloablUsings.cs
@@ -1,3 +1,6 @@
-﻿global using Fakrny.Infrastructure;
+﻿global using Fakrny.Application.Interfaces.Services;
+global using Fakrny.Domain.Dtos;
+global using Fakrny.Domain.Entities;
+global using Fakrny.Infrastructure;
 global using Microsoft.AspNetCore.Mvc;
 
